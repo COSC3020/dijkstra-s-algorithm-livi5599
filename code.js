7348248de75ba019445e3a361d45d30603a47476
livi5599 <obrown1@uwyo.edu>
2024-12-10 23:38:58 -0700

    Update code.js

--- code.js
+++ code.js
@@ -15,29 +15,34 @@ function dijkstra(graph, sourceNode) {
         let currentNode = null; //Represents the node to be processed next. Set to null since no node is selected yet
         let minDist = Infinity; //Represents the shortest known distance from sourceNode among unvisited nodes. Set to infinity since it is assumed that neighbors are unreachable
         for (column = 0; column < unvisited.length; column++) { //Iterates over all unvisited nodes
-            //console.log(graph[sourceNode][column]);
-            //console.log("graph[sourceNode] = ", graph[sourceNode]);
-            //console.log("unvisited = ", unvisited);
-            //console.log("unvisited[column] = ", unvisited[column]);
-            //console.log(unvisited.includes(unvisited[column]));
-            //console.log(shortestDist[column]);
-            //console.log("unvisited[column] = ", unvisited[column]);
             if (shortestDist[unvisited[column]] < minDist) { //If the shortest distance to the current unvisited node is less than the shortest known distance from sourceNode to its neighbor...
-                minDist = shortestDist[column]; //Set the shortest known distance to the distance of this unvisited node
-                currentNode = column; //Set currentNode to this unvisited node
+                minDist = shortestDist[unvisited[column]]; //Set the shortest known distance to the distance of this unvisited node
+                currentNode = unvisited[column]; //Set currentNode to this unvisited node
+                // if (currentNode == null) {
+                //     currentNode = -1;
+                // }
                 console.log("minDist after if = ", minDist);
                 console.log("currentNode after if = ", currentNode);
             }
+            
+            // if (unvisited.includes(unvisited[column]) && shortestDist[column] > 0) {
+            //     if (graph[sourceNode][column] + shortestDist[sourceNode] < shortestDist[column]) {
+            //         shortestDist[column] = graph[sourceNode][column] + shortestDist[sourceNode];
+            //         console.log("shortestDist = ", shortestDist);
+            //     }
+            // }
         }
 
         unvisited = unvisited.filter(node => node !== currentNode); //Remove the node being processed next (currentNode) from unvisited, as it will now be processed
 
         for (column = 0; column < unvisited.length; column++) { //Iterates over each unvisited neighbor of sourceNode
-            if (graph[currentNode][column] + shortestDist[column] < minDist) { //If the distance from the node being processed and its neighbor + the shortest distance from sourceNode to its neighbor is less than the shortest known distance from sourceNode...
-                shortestDist[column] = graph[currentNode][column] + shortestDist[column]; //Set the shortest distance from sourceNode to its neighbor to the sum of the distance from the node being processed to the neighbor node and the shortest distance from sourceNode to its neighbor
+            console.log("currentNode = ", currentNode);
+            console.log("graph[currentNode] = ", graph[currentNode]);
+
+            if (graph[currentNode][unvisited[column]] !== undefined && shortestDist[currentNode] + graph[currentNode][unvisited[column]] < shortestDist[unvisited[column]]) { //If the distance from the node being processed and its neighbor + the shortest distance from sourceNode to its neighbor is less than the shortest known distance from sourceNode...
+                shortestDist[unvisited[column]] = shortestDist[currentNode] + graph[currentNode][unvisited[column]]; //Set the shortest distance from sourceNode to its neighbor to the sum of the distance from the node being processed to the neighbor node and the shortest distance from sourceNode to its neighbor
                 console.log("shortestDist = ", shortestDist);
             }
         }
     }
     return shortestDist; //Return the shortest distances from sourceNode to all other nodes
-}
